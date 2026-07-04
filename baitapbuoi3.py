480abc38f12437fd77ea6a33b495cb7fc2dd1f61
Okahachirou1 <thaivinhle1996@gmail.com>
2026-07-04 20:26:55 +0700

    Bài tập buổi 3

--- baitapbuoi3.py
+++ baitapbuoi3.py
@@ -1,8 +1,8 @@
 password = input("Nhập mật khẩu của bạn: ")
-do_dai = len(password)
-if do_dai < 5:
-    print("Mật khẩu quá yếu! Nguy cơ bị hack rất cao")
-elif do_dai <=8:
-    print("Mật khẩu ở mức trung bình. Bạn nên đặt dài hơn.")
+if len(password) >8:
+    if "123" in password:
+        print("Mật khẩu không được chứa chuỗi '123' ")
+    else:
+        print("Mật khẩu an toàn")
 else:
-    print("Mật khẩu an toàn!")
+    print("Mật khẩu yếu")
